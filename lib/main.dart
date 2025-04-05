fd5722a574a206d2ba1e150bca692d48fe6158f2
markiu200 <markiu2007@interia.pl>
2025-04-05 16:53:55 +0200

    move start screen to StatefulWidget
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37131290#overview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,42 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          containerChild: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  width: 300,
-                  color: Color.fromARGB(150, 255, 255, 255),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 60),
-                  child: Text(
-                    'Learn Flutter the fun way!',
-                    style: TextStyle(color: Colors.white, fontSize: 28),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 40),
-                  child: OutlinedButton.icon(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                    ),
-                    icon: Icon(Icons.arrow_right_alt),
-                    label: Text('Start Quiz'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+  Widget build(BuildContext build) {
+    return Quiz();
   }
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_container.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          containerChild: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                  color: Color.fromARGB(150, 255, 255, 255),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 60),
+                  child: Text(
+                    'Learn Flutter the fun way!',
+                    style: TextStyle(color: Colors.white, fontSize: 28),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                    ),
+                    icon: Icon(Icons.arrow_right_alt),
+                    label: Text('Start Quiz'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
