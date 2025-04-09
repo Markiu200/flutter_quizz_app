193ee1c0ff41a89409a4f716ba84f704fc55b5cc
markiu200 <markiu2007@interia.pl>
2025-04-09 22:25:18 +0200

    Change MyApp to be StatefulWidget and be able to change screens
    Problem with this is that entire MaterialApp is being rebuilt for each screen. Udemy tutorial made it bit more nested.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/questions_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget? screen;
+
+  @override
+  void initState() {
+    super.initState(); //always paired with function, put on top
+    // couldn't do it outside initState - method is being created still
+    screen = Quiz(switchScreen);
+  }
+
+  void switchScreen() {
+    setState(() {
+      screen = QuestionsScreen();
+    });
+  }
+
+  @override
   Widget build(BuildContext build) {
-    return Quiz();
+    return screen!;
   }
 }
