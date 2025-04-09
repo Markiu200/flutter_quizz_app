094550a4445280604d6a5352604fd3e2245617cc
markiu200 <markiu2007@interia.pl>
2025-04-09 21:58:17 +0200

    Change Quiz widget to be Stateless widget
    udemy (and 2 previous): https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37131310#overview

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/gradient_container.dart';
 
-class Quiz extends StatefulWidget {
+// class Quiz extends StatefulWidget {
+//   const Quiz({super.key});
+
+//   @override
+//   State<Quiz> createState() {
+//     return _QuizState();
+//   }
+// }
+
+class Quiz extends StatelessWidget {
   const Quiz({super.key});
 
-  @override
-  State<Quiz> createState() {
-    return _QuizState();
-  }
-}
-
-class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
