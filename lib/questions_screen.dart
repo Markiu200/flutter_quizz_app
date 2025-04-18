c522b1645fbb3f967151193c0418d2efecec27e0
markiu200 <markiu2007@interia.pl>
2025-04-18 17:07:03 +0200

    Make buttons equal width
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37131370#overview

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -22,20 +22,24 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
         body: GradientContainer(
           containerChild: SizedBox(
             width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  currentQuestion.question,
-                  style: TextStyle(color: Colors.white),
-                ),
-                // map runs a function for each list element returns Iterable of
-                // the results it got. Then spread operator (...) makes that
-                // into separate Widgets instead of list (Iterable) of Widgets.
-                ...currentQuestion.answers.map((answer) {
-                  return AnswerButton(answer, () {});
-                }),
-              ],
+            child: Container(
+              margin: const EdgeInsets.all(40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    currentQuestion.question,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  // map runs a function for each list element returns Iterable of
+                  // the results it got. Then spread operator (...) makes that
+                  // into separate Widgets instead of list (Iterable) of Widgets.
+                  ...currentQuestion.answers.map((answer) {
+                    return AnswerButton(answer, () {});
+                  }),
+                ],
+              ),
             ),
           ),
         ),
