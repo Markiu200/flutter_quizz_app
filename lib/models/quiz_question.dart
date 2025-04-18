02fad678c1f73f58f35f282ee8319b33af31fed6
markiu200 <markiu2007@interia.pl>
2025-04-18 17:18:45 +0200

    Add getShuffledList method to QuizQuestion
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37131398#overview

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,4 +2,12 @@ class QuizQuestion {
   const QuizQuestion(this.question, this.answers);
   final String question;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    // Keeping original list intact because first answer is always correct -
+    // this will be used to summarize the quiz.
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
