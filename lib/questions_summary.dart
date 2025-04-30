c752eaa18c757dec0fc284f5e74c04ed0b37c0d2
markiu200 <markiu2007@interia.pl>
2025-04-30 20:25:45 +0200

    Make shown answers scrollable, add padding
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37131528#overview

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,25 +7,33 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:
-          summaryData.map((data) {
-            return Row(
-              children: [
-                Text(((data["question_index"] as int) + 1).toString()),
-                Expanded(
-                  child: Column(
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50),
+          child: Column(
+            children:
+                summaryData.map((data) {
+                  return Row(
                     children: [
-                      Text(data["question"] as String),
-                      const SizedBox(height: 5),
-                      Text(data["selectedAnswer"] as String),
-                      Text(data["correctAnswer"] as String),
+                      Text(((data["question_index"] as int) + 1).toString()),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text(data["question"] as String),
+                            const SizedBox(height: 5),
+                            Text(data["selectedAnswer"] as String),
+                            Text(data["correctAnswer"] as String),
+                          ],
+                        ),
+                      ),
                     ],
-                  ),
-                ),
-              ],
-            );
-          }).toList(),
+                  );
+                }).toList(),
+          ),
+        ),
+      ),
     );
   }
 }
