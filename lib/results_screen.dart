0e36b7d6408322118d42afbeab87dba46918ca73
markiu200 <markiu2007@interia.pl>
2025-04-30 17:42:41 +0200

    Add getSummaryData() that returns data for results screen
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37131460#overview

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen(this.selectedAnswers, {super.key});
 
   final List<String> selectedAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (int i = 0; i < selectedAnswers.length; i++) {
+      summary.add({
+        "question_index": i,
+        "question": questions[i].question,
+        "correctAnswer": questions[i].answers[0],
+        "selectedAnswer": selectedAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
